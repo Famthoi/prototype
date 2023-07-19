7da7e1d515b7e68e478f34ddccafc0aea85e7782
Famthoi <mf5172@srmist.edu.in>
2023-07-19 15:48:27 -0400

    PowerUps

--- Assets/ScriptsX/PlayerControllerX.cs
+++ Assets/ScriptsX/PlayerControllerX.cs
@@ -5,17 +5,43 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     Rigidbody playerRb;
+    GameObject focalPoint;
     public float speed = 5.0f;
+    public bool hasPowerUp;
+    float powerupStrength = 15.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerUp);
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * forwardInput * speed);
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
     }
 }
